874f8db58620296928bc8d81930e3ff4c52f3010
Navendu Sanwal <navendu.bsf@gmail.com>
2021-05-06 23:48:39 +0530

    Literal Types

--- app.ts
+++ app.ts
@@ -1,19 +1,33 @@
 // Union Types
 
 // should work with numbers and string
-function combine(a: number | string, b: number | string) {
+function combine(
+  a: number | string,
+  b: number | string,
+  resultConversion: 'as-number' | 'as-text'
+) {
   let result;
 
-  if (typeof a === 'number' && typeof b === 'number') {
-    result = a + b;
+  if ((typeof a === 'number' && typeof b === 'number') || resultConversion === 'as-number') {
+    result = +a + +b;
   } else {
     result = a.toString() + b.toString();
-    return result;
   }
+
+  // if (resultConversion === 'as-number') {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
+
+  return result;
 }
 
-const combineAges = combine(30, 25);
+const combineAges = combine(30, 25, 'as-number');
 console.log('combineAges: ', combineAges);
 
-const combineNames = combine('Peter', 'Parker');
+const combineStringAges = combine('30', '25', 'as-number');
+console.log('combineStringAges: ', combineStringAges);
+
+const combineNames = combine('Peter', 'Parker', 'as-text');
 console.log('combineNames: ', combineNames);
