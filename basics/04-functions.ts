c42a25b01ad2d35427bedf18c7417c03dd78f14f
Navendu Sanwal <navendu.bsf@gmail.com>
2021-05-07 00:43:24 +0530

    Clean up

--- basics/04-functions.ts
+++ basics/04-functions.ts
@@ -1,9 +1,9 @@
-function add(a: number, b: number) {
+function add2(a: number, b: number) {
   // Return type is inferred as number
   return a + b;
 }
 
-function printResult(num: number): void {
+function printResult2(num: number): void {
   // similarly, void is also inferred
   console.log('num: ', num);
 }
@@ -15,13 +15,13 @@ function addAndHandle(a: number, b: number, cb: (num: number) => void) {
 
 // ---------
 
-printResult(add(5, 12));
+printResult2(add2(5, 12));
 
 // Functions as Types
 
 let combineValues: (a: number, b: number) => number;
 
-combineValues = add;
+combineValues = add2;
 // combineValues = 5; // Valid, if combineValues type is any, so TS will pass it, but it will fail in runtime since combineValues is no longer a function
 // combineValues = printResult; // Valid, if combineValues type is Function, but will fail logic
 
