37528891c79e3dde31c9241f06a7437acc71743b
Navendu Sanwal <navendu.bsf@gmail.com>
2021-05-06 23:10:55 +0530

    Array Types

--- app.ts
+++ app.ts
@@ -9,12 +9,19 @@
 // console.log('person: ', person.nickname); // Property 'name' does not exist on type 'object'.
 
 // Correct
-const person: {
-  name: string;
-  age: number;
-} = {
+const person = {
   name: 'Peter Parker',
   age: 20,
+  hobbies: ['crawling', 'slinging'],
 };
 
+let favoriteActivities: string[];
+// favoriteActivities = 'Sports'; // Type 'string' is not assignable to type 'string[]'.
+favoriteActivities = ['Sports'];
+
 console.log('person: ', person.name);
+
+for (const hobby of person.hobbies) {
+  console.log('hobby: ', hobby.toUpperCase());
+  // console.log('hobby: ', hobby.map()); // Property 'map' does not exist on type 'string'.
+}
