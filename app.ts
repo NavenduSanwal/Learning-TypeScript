3ebf632e7ea615b29f01eeff62146a949b2f66b6
Navendu Sanwal <navendu.bsf@gmail.com>
2021-05-07 00:09:33 +0530

    Functions as Types

--- app.ts
+++ app.ts
@@ -3,9 +3,19 @@ function add(a: number, b: number) {
   return a + b;
 }
 
-function print(num: number): void {
+function printResult(num: number): void {
   // similarly, void is also inferred
   console.log('num: ', num);
 }
 
-print(add(5, 12));
+printResult(add(5, 12));
+
+// Functions as Types
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = 5; // Valid, if combineValues type is any, so TS will pass it, but it will fail in runtime since combineValues is no longer a function
+// combineValues = printResult; // Valid, if combineValues type is Function, but will fail logic
+
+console.log(combineValues(10, 10));
