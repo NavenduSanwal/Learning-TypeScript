a771718f0e68ca18109ab0bd7283b80aa26ac0d6
Navendu Sanwal <navendu.bsf@gmail.com>
2021-06-13 22:31:37 +0530

    Using Interface with classes

--- src/classes-iInterfaces/02-interfaces.ts
+++ src/classes-iInterfaces/02-interfaces.ts
@@ -1,18 +1,28 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
   greet(phrase: string): void;
 }
 
-// use interface as a type checking
-let user1: Person;
+// can inherit from only 1 class, but can implements multiple interfaces
+// interfaces are often used to share functionality amongst different classes
+// It's a bit like working with abstract class system for the difference being that an interface has no
+// implementation details at all, whereas abstract classes can be a mixture of you have to overwrite this parts and I have a concrete implementation parts.
+class Person implements Greetable {
+  name: string;
+  age = 30;
 
-user1 = {
-  name: 'John',
-  age: 20,
+  constructor(n: string) {
+    this.name = n;
+  }
+
   greet(phrase: string) {
     console.log(phrase + ' ' + this.name);
-  },
-};
+  }
+}
+
+// use interface as a type checking
+let user1: Greetable; // Greetable also works since Person implements Greetable, therefore it will to comply with the interface
+
+user1 = new Person('John');
 
 user1.greet('Hi there, I am');
